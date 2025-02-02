74703836b70c7e7e39bcfb07580f4bc451d8c569
Vadim Molchanov <vadim.molchanov@gmail.com>
2025-02-02 10:21:56 +0000

    fix isTrue

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -24,7 +24,7 @@ suite('Unit Tests', function () {
     test('#isTrue, #isNotTrue', function () {
       assert.isTrue(true, 'true is true');
       assert.isNotTrue(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
