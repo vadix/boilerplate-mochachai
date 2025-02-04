86da3c77a2741d8fc171ec01f4461dbce9f71519
Vadim Molchanov <vadim.molchanov@gmail.com>
2025-02-04 09:04:29 +0000

    fix

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -47,7 +47,7 @@ suite('Unit Tests', function () {
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
-      assert.notDeepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
+      assert.deepEqual({ a: '1', b: 5 }, { b: 5, a: '1' }, "The order of keys doesn't matter");
       assert.deepEqual({ a: [5, 6] }, { a: [6, 5] }, 'The order of array elements does matter');
     });
   });
